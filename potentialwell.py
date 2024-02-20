0ee9cfae288c75f2eb48b39271cbf3aea08fc3e6
Riddhi0109 <159097333+Riddhi0109@users.noreply.github.com>
2024-02-20 10:03:04 +0530

    Update potentialwell.py

--- potentialwell.py
+++ potentialwell.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.title('Quantum Mechanics')
 st.header('1-D Potential Well',divider='rainbow')
 
-a=4
+a=st.number_input('Enter the Length of well')
 x=np.linspace(-a,a,10000)
 
 def psi(x, n):
@@ -18,7 +18,7 @@ st.text(n)
 plt.plot(x,(psi(x,n))**2,'--',label=rf'$\psi_{n}$')
 plt.ylabel('$\psi(x)^2$')
 plt.xlabel('x')
-plt.xlim(0,4)
+plt.xlim(0,a)
 plt.ylim(bottom=0)
 plt.savefig('graph.jpg')
 st.image('graph.jpg')
