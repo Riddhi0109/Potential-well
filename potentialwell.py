dbf836c3a29805c1c8fa85a8defbca33f9f09cbc
Riddhi0109 <159097333+Riddhi0109@users.noreply.github.com>
2024-02-20 10:00:29 +0530

    Update potentialwell.py

--- potentialwell.py
+++ potentialwell.py
@@ -13,10 +13,10 @@ def psi(x, n):
     y = np.sqrt(2 / a) * np.sin((n * np.pi * x) / (a))
     return y
 
-#n=int(input('enter the state:-'))
+
 n=st.number_input('Enter the state')
 st.text(n)
-#print(x,psi(x,n))
+
 plt.plot(x,(psi(x,n))**2,'--',label=rf'$\psi_{n}$')
 
 plt.ylabel('$\psi(x)^2$')
@@ -25,5 +25,5 @@ plt.xlim(0,4)
 plt.ylim(bottom=0)
 plt.savefig('graph.jpg')
 st.image('graph.jpg')
-#plt.show()
+
 
