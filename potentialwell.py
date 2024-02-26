6c8ba8c7920236d3e9400d7b268421a136dd5d9b
Riddhi0109 <159097333+Riddhi0109@users.noreply.github.com>
2024-02-27 00:17:52 +0530

    Update potentialwell.py

--- potentialwell.py
+++ potentialwell.py
@@ -1,21 +1,22 @@
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import streamlit as st
 
 st.title('Quantum Mechanics')
 st.header('1-D Potential Well',divider='rainbow')
 
-st.caption("A particle in a 1-dimensional box is a fundamental quantum mechanical approximation describing the translational motion of a single particle confined inside an infinitely deep well from which it cannot escape.")
-st.caption("The potential energy is 0 inside the box (V=0 for 0<x<L) and goes to infinity at the walls of the box (V=∞ for x<0 or x>L).")
+st.text("A particle in a 1-dimensional box is a fundamental quantum mechanical approximation describing the translational motion of a single particle confined inside an infinitely deep well from which it cannot escape.")
+st.text("The potential energy is 0 inside the box (V=0 for 0<x<L) and goes to infinity at the walls of the box (V=∞ for x<0 or x>L).")
 
-a=st.slider('Enter the Length of well',1,20)
+a=st.slider('Enter the length of well',1,20)
 x=np.linspace(-a,a,10000)
+
 n=st.slider('Enter the state',1,100)
 
 def psi(x, n):
     y = np.sqrt(2 / a) * np.sin((n * np.pi * x) / (a))
     return y
-    
+
 def psi1(x, n):
     y1 = np.sqrt(1 / (a)) * np.sin((n * np.pi * x) / (a))
     return y1
@@ -25,6 +26,7 @@ def psi2(x,n):
     return y2
     
 options = st.radio('choose type of well',["Asymmetric Potential Well","Symmetric Potential Well","Potential well with length 2a","Both",])
+
 if options=='Asymmetric Potential Well':
     plt.subplot(2,1,1)
     plt.plot(x,(psi(x,n)))
@@ -39,7 +41,7 @@ if options=='Asymmetric Potential Well':
     plt.savefig('graph.jpg')
     st.image('graph.jpg')
 
-if options=='Symmetric Potential Well':
+if options=='Potential well with length 2a':
     plt.subplot(2,1,1)
     plt.plot(x,(psi1(x,n)))
     plt.xlim(-a,a)
@@ -54,21 +56,23 @@ if options=='Symmetric Potential Well':
     plt.ylim(bottom=0)
     plt.savefig('graph1.jpg')
     st.image('graph1.jpg')
-
-if options=='Potential well with length 2a':
+    
+if options=='Symmetric Potential Well':
     plt.subplot(2,1,1)
-    plt.plot(x,(psi(x,n)))
-    plt.xlim(0,a)
+    plt.plot(x,(psi2(x,n)))
+    plt.xlim(-a,a)
     plt.axhline(0,color='black')
+    plt.axvline(0,color='black')
     plt.subplot(2,1,2)
-    plt.plot(x,(psi(x,n))**2)
-    plt.ylabel('$\psi(x)^2$')
+    plt.plot(x,(psi2(x,n))**2)
+    plt.ylabel('$\psi1(x)^2$')
     plt.xlabel('x')
-    plt.xlim(0,a)
+    plt.xlim(-a,a)
+    plt.axvline(0,color='black')
     plt.ylim(bottom=0)
-    plt.savefig('graph.jpg')
-    st.image('graph.jpg')
-
+    plt.savefig('graph1.jpg')
+    st.image('graph1.jpg')
+    
 if options=='Both':
     
     plt.subplot(2,1,1)
@@ -99,5 +103,26 @@ if options=='Both':
     #plt.tight_layout()
     plt.savefig('graph1.jpg')
     st.image('graph1.jpg')
+    
+    plt.subplot(2,1,1)
+    plt.plot(x,(psi2(x,n)))
+    plt.xlim(-a,a)
+    plt.axhline(0,color='black')
+    plt.axvline(0,color='black')
+    plt.subplot(2,1,2)
+    plt.plot(x,(psi2(x,n))**2)
+    plt.ylabel('$\psi1(x)^2$')
+    plt.xlabel('x')
+    plt.xlim(-a,a)
+    plt.axvline(0,color='black')
+    plt.ylim(bottom=0)
+    plt.savefig('graph1.jpg')
+    st.image('graph1.jpg')
+
+
+
+
+
+   
 
 
